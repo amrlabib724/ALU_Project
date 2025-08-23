c542a34442dece9af9b8d2304c872697d24f2590
amr labib <amrlabib724@gmail.com>
2025-08-23 10:15:15 +0300

    Update ALU_TOP.v

--- ALU_Project/ALU_TOP.v
+++ ALU_Project/ALU_TOP.v
@@ -13,31 +13,16 @@ wire [3:0] seven_seg_in;
 wire [3:0] Letters;
 wire [1:0] sel_in; 
 wire pb_a_debounced, pb_b_debounced, pb_op_debounced,slow_clk;
-// wire [7:0] sw_debounced;
-
 
 
 // Clk Divider for slower clock signal
 Clk_div #(.counter_div(25'd4)) clk_div_inst (.Clk(Clk), .Reset(reset), .Clk_out(slow_clk));
 
-
-
 //Instatiate debounce modules for buttons and switches
 
-// Debouncer reset_debounce (.Clk(slow_clk), .Reset(1'b0), .pb_in(reset), .pb_out(reset_debounced));
 Debouncer pb_a_debounce (.Clk(slow_clk), .Reset(reset), .pb_in(pb_a), .pb_out(pb_a_debounced));
 Debouncer pb_b_debounce (.Clk(slow_clk), .Reset(reset), .pb_in(pb_b), .pb_out(pb_b_debounced));
 Debouncer pb_op_debounce(.Clk(slow_clk), .Reset(reset), .pb_in(pb_op), .pb_out(pb_op_debounced));
-// Debouncer sw_debounce_0 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[0]), .pb_out(sw_debounced[0]));
-// Debouncer sw_debounce_1 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[1]), .pb_out(sw_debounced[1]));
-// Debouncer sw_debounce_2 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[2]), .pb_out(sw_debounced[2]));
-// Debouncer sw_debounce_3 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[3]), .pb_out(sw_debounced[3]));
-// Debouncer sw_debounce_4 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[4]), .pb_out(sw_debounced[4]));
-// Debouncer sw_debounce_5 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[5]), .pb_out(sw_debounced[5]));
-// Debouncer sw_debounce_6 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[6]), .pb_out(sw_debounced[6]));
-// Debouncer sw_debounce_7 (.Clk(slow_clk), .Reset(reset), .pb_in(sw[7]), .pb_out(sw_debounced[7]));
-
-
 
 //Instantiate ALU module
 ALU alu_inst (
@@ -52,14 +37,11 @@ ALU alu_inst (
     .LED(LED)
 );
 
-
 // Instantiate DEC_To_BCD module
 DEC_To_BCD dec_to_bcd_inst (.BINARY(Out_with_carry),.BCD(BCD_out));
 
-
 // Instantiate AN_SEL module
 AN_SEL an_sel_inst(.Clk(slow_clk), .Reset(reset), .AN(AN_SEL), .sel(sel_in));
-
 
 // Instantiate mux_control model
 mux_4x1_control mux_control_inst (
@@ -71,12 +53,10 @@ mux_4x1_control mux_control_inst (
     .digit_BCD(seven_seg_in)
 );
 
-
 // Instantiate seven_seg_decode module
 seven_seg_decode seven_seg_decode_inst (
     .digit_BCD(seven_seg_in),
     .seven_seg(seven_seg_out)
 );
-
 
 endmodule //ALU_TOP
