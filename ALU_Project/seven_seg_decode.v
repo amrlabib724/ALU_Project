2f242809264c4e23dc418715a43e523b45376ca8
amr labib <amrlabib724@gmail.com>
2025-08-28 03:19:01 +0300

    Add_seven_seg_decode

--- ALU_Project/seven_seg_decode.v
+++ ALU_Project/seven_seg_decode.v
@@ -3,20 +3,20 @@ module seven_seg_decode (digit_BCD,seven_seg);
  output reg [6:0] seven_seg;
      always @(*) begin
         case (digit_BCD)
-            4'd0: seven_seg = 7'b1111110;
-            4'd1: seven_seg = 7'b0110000;
-            4'd2: seven_seg = 7'b1101101;
-            4'd3: seven_seg = 7'b1111001;
-            4'd4: seven_seg = 7'b0110011;
-            4'd5: seven_seg = 7'b1011011;
-            4'd6: seven_seg = 7'b1011111;
-            4'd7: seven_seg = 7'b1110000;
-            4'd8: seven_seg = 7'b1111111;
-            4'd9: seven_seg = 7'b1111011;
-            4'hA: seven_seg = 7'b1110111; 
-            4'hB: seven_seg = 7'b0011111; 
-            4'hC: seven_seg = 7'b1001110; 
-          default: seven_seg = 7'b0000000; 
+            4'd0: seven_seg = 7'b0000001; // 0
+            4'd1: seven_seg = 7'b1001111; // 1
+            4'd2: seven_seg = 7'b0010010; // 2
+            4'd3: seven_seg = 7'b0000110; // 3
+            4'd4: seven_seg = 7'b1001100; // 4
+            4'd5: seven_seg = 7'b0100100; // 5
+            4'd6: seven_seg = 7'b0100000; // 6
+            4'd7: seven_seg = 7'b0001111; // 7
+            4'd8: seven_seg = 7'b0000000; // 8
+            4'd9: seven_seg = 7'b0000100; // 9
+            4'hA: seven_seg = 7'b0001000; // A
+            4'hB: seven_seg = 7'b1100000; // B
+            4'hC: seven_seg = 7'b0110001; // C
+          default: seven_seg = 7'b1111111; // default
         endcase
     end
 endmodule //seven_seg_decode
--- ALU_Project/seven_seg_decode_testbench.v
+++ ALU_Project/seven_seg_decode_testbench.v
@@ -10,21 +10,21 @@ initial begin
     for (i=0;i<20 ;i=i+1) begin
         digit_BCD=$random % 13 ;
         #10; 
-        case (digit_BCD)
-            4'd0: if (seven_seg !== 7'b1111110)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd1: if (seven_seg !== 7'b0110000)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd2: if (seven_seg !== 7'b1101101)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd3: if (seven_seg !== 7'b1111001)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd4: if (seven_seg !== 7'b0110011)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd5: if (seven_seg !== 7'b1011011)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd6: if (seven_seg !== 7'b1011111)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd7: if (seven_seg !== 7'b1110000)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd8: if (seven_seg !== 7'b1111111)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'd9: if (seven_seg !== 7'b1111011)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'hA: if (seven_seg !== 7'b1110111)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'hB: if (seven_seg !== 7'b0011111)begin $display("Error for digit %d", digit_BCD);$stop;end
-            4'hC: if (seven_seg !== 7'b1001110)begin $display("Error for digit %d", digit_BCD);$stop;end
-          default: if (seven_seg !== 7'b0000000)begin $display("Error for invalid input %d", digit_BCD);$stop;end
+                case (digit_BCD)
+                        4'd0: if (seven_seg !== 7'b0000001)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd1: if (seven_seg !== 7'b1001111)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd2: if (seven_seg !== 7'b0010010)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd3: if (seven_seg !== 7'b0000110)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd4: if (seven_seg !== 7'b1001100)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd5: if (seven_seg !== 7'b0100100)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd6: if (seven_seg !== 7'b0100000)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd7: if (seven_seg !== 7'b0001111)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd8: if (seven_seg !== 7'b0000000)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'd9: if (seven_seg !== 7'b0000100)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'hA: if (seven_seg !== 7'b0001000)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'hB: if (seven_seg !== 7'b1100000)begin $display("Error for digit %d", digit_BCD);$stop;end
+                        4'hC: if (seven_seg !== 7'b0110001)begin $display("Error for digit %d", digit_BCD);$stop;end
+                    default: if (seven_seg !== 7'b1111111)begin $display("Error for invalid input %d", digit_BCD);$stop;end
         endcase
     end
     $display("All tests passed!");
